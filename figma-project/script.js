6016b2aacceec3980ca35a2c9b7411c502737608
Ankit Gupta <ankit.g.1288@prometteur.in>
2026-04-14 17:08:38 +0530

    Completed step-2 of signup

--- figma-project/script.js
+++ figma-project/script.js
@@ -1,11 +1,28 @@
 const signupBtn = document.querySelector('.register-btn');
 
-signupBtn.addEventListener('click', () => {
-    window.location.href = 'register.html';
-})
+if (signupBtn) {
+    signupBtn.addEventListener('click', () => {
+        window.location.href = 'register.html';
+    });
+}
 
 const loginBtn = document.querySelector('.login-btn');
 
-loginBtn.addEventListener('click', () => {
-    window.location.href = 'login.html';
-})
+if (loginBtn) {
+    loginBtn.addEventListener('click', () => {
+        window.location.href = 'login.html';
+    });
+}
+
+const signupForm = document.querySelector('.signup-form');
+const signupStep = document.getElementById('signupStep');
+const clubStep = document.getElementById('clubStep');
+
+if (signupForm && signupStep && clubStep) {
+    signupForm.addEventListener('submit', function (e) {
+        e.preventDefault();
+
+        signupStep.style.display = 'none';
+        clubStep.style.display = 'block';
+    });
+}
